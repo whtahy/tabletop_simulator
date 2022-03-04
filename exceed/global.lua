5ed91ccdcbda5cda996ecceeaec24c4406f5a7bf
whtahy <whtahy@users.noreply.github.com>
2022-03-03 20:27:26 -0500

    Initial cleanup for exceed/global.lua

--- exceed/global.lua
+++ exceed/global.lua
@@ -1,13 +1,10 @@
-p1_gauge_zone_guid = '04d10f'
-p2_gauge_zone_guid = '52cac8'
+p1_gauge_zone = getObjectFromGUID('04d10f')
+p2_gauge_zone = getObjectFromGUID('52cac8')
 
-p1_gauge_text_guid = '9edd50'
-p2_gauge_text_guid = 'ebca94'
+p1_gauge_text = getObjectFromGUID('9edd50')
+p2_gauge_text = getObjectFromGUID('ebca94')
 
 function onLoad()
-    loadZones()
-    loadTexts()
-
     for _, obj in ipairs(getAllObjects()) do
         if obj.hasTag('Static') or obj.hasTag('Table') then
             obj.interactable = false
@@ -16,52 +13,35 @@ function onLoad()
     end
 end
 
---Gauge Logic
-function loadZones()
-    p1_gauge_zone = getObjectFromGUID(p1_gauge_zone_guid)
-    p2_gauge_zone = getObjectFromGUID(p2_gauge_zone_guid)
+function onObjectEnterZone(zone, object)
+    if zone == p1_gauge_zone
+        or zone == p2_gauge_zone then
+        update_gauge(p1_gauge_zone)
+        update_gauge(p2_gauge_zone)
+    end
 end
 
-function loadTexts()
-    p1_gauge_text = getObjectFromGUID(p1_gauge_text_guid)
-    p2_gauge_text = getObjectFromGUID(p2_gauge_text_guid)
+function onObjectLeaveZone(zone, object)
+    if zone == p1_gauge_zone
+        or zone == p2_gauge_zone then
+        update_gauge(p1_gauge_zone)
+        update_gauge(p2_gauge_zone)
+    end
 end
 
-function countCardsInZone(zone)
-    local count=0
-    for i, lobj in ipairs(zone.getObjects()) do
-      if lobj.type == "Card" then
-        count = count + 1
-      elseif lobj.type == "Deck" then
-        count = count + #lobj.getObjects()
-      end
+function update_gauge(zone)
+    local count = 0
+    for _, obj in ipairs(zone.getObjects()) do
+        if obj.type == "Card" then
+            count = count + 1
+        elseif obj.type == "Deck" then
+            count = count + #obj.getObjects()
+        end
     end
 
-    if p2_gauge_zone == zone then
-        p2_gauge_text.TextTool.setValue(tostring(count))
-    elseif p1_gauge_zone == zone then
+    if zone == p1_gauge_zone then
         p1_gauge_text.TextTool.setValue(tostring(count))
+    elseif zone == p2_gauge_zone then
+        p2_gauge_text.TextTool.setValue(tostring(count))
     end
 end
-
-function refreshcount()
-    countCardsInZone(p1_gauge_zone)
-    countCardsInZone(p2_gauge_zone)
-end
-
-function onObjectEnterZone(curzone,obj)
-    if p2_gauge_zone == curzone then
-        refreshcount()
-    elseif p1_gauge_zone == curzone then
-        refreshcount()
-    end
-end
-
-function onObjectLeaveZone(curzone,obj)
-    if p2_gauge_zone == curzone then
-        refreshcount()
-    elseif p1_gauge_zone == curzone then
-        refreshcount()
-    end
-end
---End of Gauge Logic
