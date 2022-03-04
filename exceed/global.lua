bd31ebdd3f529b81ddc0d3e7374a0db9ad30b4ab
whtahy <whtahy@users.noreply.github.com>
2022-03-03 21:43:46 -0500

    Exceed: refactor discard code in global.lua

--- exceed/global.lua
+++ exceed/global.lua
@@ -1,10 +1,16 @@
-p1_gauge_zone = getObjectFromGUID('04d10f')
-p2_gauge_zone = getObjectFromGUID('52cac8')
-
-p1_gauge_text = getObjectFromGUID('9edd50')
-p2_gauge_text = getObjectFromGUID('ebca94')
+-- NOTE: can't use getObjectFromGUID in naked global
 
 function onLoad()
+    gauge_zones_table = {
+        Red = getObjectFromGUID('f1a600'),
+        Blue = getObjectFromGUID('059d91')
+    }
+
+    gauge_text_table = {
+        Red = getObjectFromGUID('36b535'),
+        Blue = getObjectFromGUID('ffed4f')
+    }
+
     for _, obj in ipairs(getAllObjects()) do
         if obj.hasTag('Static') or obj.hasTag('Table') then
             obj.interactable = false
@@ -14,34 +20,36 @@ function onLoad()
 end
 
 function onObjectEnterZone(zone, object)
-    if zone == p1_gauge_zone
-        or zone == p2_gauge_zone then
-        update_gauge(p1_gauge_zone)
-        update_gauge(p2_gauge_zone)
-    end
+    update_gauges(zone, object)
 end
 
 function onObjectLeaveZone(zone, object)
-    if zone == p1_gauge_zone
-        or zone == p2_gauge_zone then
-        update_gauge(p1_gauge_zone)
-        update_gauge(p2_gauge_zone)
+    update_gauges(zone, object)
+end
+
+function update_gauges(zone, object)
+    -- not a card or deck -> no need to update
+    if object.type ~= 'Card' and object.type ~= 'Deck' then
+        return nil
+    end
+
+    -- otherwise, update the player's gauge
+    if zone == gauge_zones_table['Red'] then
+        update_gauge('Red')
+    elseif zone == gauge_zones_table['Blue'] then
+        update_gauge('Blue')
     end
 end
 
-function update_gauge(zone)
+function update_gauge(player_color)
+    local gauge_objects = gauge_zones_table[player_color].getObjects()
     local count = 0
-    for _, obj in ipairs(zone.getObjects()) do
-        if obj.type == "Card" then
+    for _, obj in ipairs(gauge_objects) do
+        if obj.type == 'Card' then
             count = count + 1
-        elseif obj.type == "Deck" then
+        elseif obj.type == 'Deck' then
             count = count + #obj.getObjects()
         end
     end
-
-    if zone == p1_gauge_zone then
-        p1_gauge_text.TextTool.setValue(tostring(count))
-    elseif zone == p2_gauge_zone then
-        p2_gauge_text.TextTool.setValue(tostring(count))
-    end
+    gauge_text_table[player_color].TextTool.setValue(tostring(count))
 end
